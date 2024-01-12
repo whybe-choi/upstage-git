c116a1efdb9bda7b63c62972e2aa0b1ec6ea9211
whybe-choi <choi980912@khu.ac.kr>
2024-01-12 16:43:34 +0900

    refactor : Do fizzbuzz with 1 if Statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,10 @@
 def do_fizzbuzz():
-	for i in range(1, 15+1):
-		if i%15==0:
-			print('fizzbuzz')
-		elif i%3==0:
-			print('fizz')
-		elif i%5==0:
-			print('buzz')
+	for i in range(1,15+1):
+		 if i%3==0 or i%5==0:
+			print('fizz'*(i%3==0)+'buzz'*(i%5==0))
 		else:
 			print(f'{i}')
 
-if __name__ == '__main__':
+
+if __name__=='__main__':
 	do_fizzbuzz()
